dc9c70275ea499d4dd826fc2b9e64c808fed467a
Orchard CMS net <blazor@orchardcms.net>
2020-01-01 15:08:56 +0000

    Added MyOrchardCoreCMS

---
+++ MyOrchardCoreCMS/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace MyOrchardCoreCMS
+{
+    public class Program
+    {
+        public static Task Main(string[] args)
+            => BuildHost(args).RunAsync();
+
+        public static IHost BuildHost(string[] args)
+        {
+            var host = Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(logging => logging.ClearProviders())
+                .ConfigureWebHostDefaults(webBuilder => webBuilder
+                        .UseStartup<Startup>())
+                .Build();
+
+            return host;
+        }
+    }
+}
